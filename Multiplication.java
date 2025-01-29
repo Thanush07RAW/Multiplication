2d59202cf7d6ddcb3ee601a73a49f471ae7ac322
Thanush07RAW <draken0067@gmail.com>
2025-01-30 01:06:52 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Multiplication {
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int x=scanner.nextInt();
+		for(int i=1;i<=10;i++) {
+			System.out.println(x+" * "+i+" = "+x*i);
+		}
+	}
+}
